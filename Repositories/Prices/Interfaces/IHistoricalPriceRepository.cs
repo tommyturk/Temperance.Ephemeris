d7c59e1c07290cbb8e4d3c33d46792c0eb92ee85
Tommy Memed <tommymemed@gmail.com>
2025-11-23 13:22:29 +0000

    bump version

--- Repositories/Prices/Interfaces/IHistoricalPriceRepository.cs
+++ Repositories/Prices/Interfaces/IHistoricalPriceRepository.cs
@@ -1,4 +1,5 @@
-﻿using Temperance.Ephemeris.Models.Prices;
+﻿using Temperance.Ephemeris.Models.BackFill;
+using Temperance.Ephemeris.Models.Prices;
 
 namespace Temperance.Ephemeris.Repositories.Prices.Interfaces
 {
@@ -8,5 +9,6 @@ namespace Temperance.Ephemeris.Repositories.Prices.Interfaces
         Task<bool> SecurityDataCheck(string symbol, string interval, DateTime startDate);
         Task<List<PriceModel>> GetMonthlyDataCoverage(string symbol, string interval, DateTime startDate, DateTime endDate);
         Task<bool> DoesDailyDataExistAsync(string symbol);
+        Task<IEnumerable<SecurityDataCoverageModel>> GetMonthlyDataCoverageAsync(string symbol, string interval);
     }
 }
